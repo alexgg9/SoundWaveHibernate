ba9ced1be4681218beb82505d583fef8817f717a
alexgg9 <alexgalvezgarciaqt@gmail.com>
2023-11-24 18:06:10 +0100

    DependenciasYConexiónHibernate

---
+++ src/main/java/accesoadatos/soundwaveproject/model/SQLConnection/Connection.java
@@ -0,0 +1,31 @@
+package accesoadatos.soundwaveproject.model.SQLConnection;
+
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+public class Connection {
+
+    private static EntityManagerFactory emf;
+    private static Connection _newInstance;
+
+    private Connection() {
+        emf = Persistence.createEntityManagerFactory("sql");
+        if (emf == null) {
+            System.out.println("No se puede establecer la conexión");
+        }
+    }
+
+    public static EntityManagerFactory getConnect() {
+        if (_newInstance == null) {
+            _newInstance = new Connection();
+        }
+        return emf;
+    }
+
+    public static void close() {
+        if (emf != null) {
+            emf.close();
+        }
+    }
+
+}
